036ad3023547154b96fba4128db639468540e597
Jatin Chaudhary <09jatin2005@gmail.com>
2025-06-21 12:40:33 +0530

    redis connection issue resolved

--- Backend/src/redis/redisClient.js
+++ Backend/src/redis/redisClient.js
@@ -1,9 +1,21 @@
 import { createClient } from "redis";
+import dotenv from "dotenv";
+dotenv.config();
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
+});
 
-const redisClient = createClient({ url: process.env.REDIS_URL });
-
+console.log("process.env.REDIS_URL", process.env.REDIS_URL);
 redisClient.on("error", (err) => {
   console.error("❌ Redis Client Error:", err);
+});
+
+redisClient.on("ready", () => {
+  console.log("🚀 Redis client connected & ready!");
 });
 
 await redisClient.connect();
